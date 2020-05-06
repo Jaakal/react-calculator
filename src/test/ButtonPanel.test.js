c6de7230f96bc6e4c43667a820ed6d9c8b9ac6f4
Jaakal <jaak.kivinukk@gmail.com>
2020-05-06 20:08:15 +0300

    Added ButtonPanel component tests

--- src/test/ButtonPanel.test.js
+++ src/test/ButtonPanel.test.js
@@ -1,29 +1,117 @@
 import React from 'react';
-import { render, queryByAttribute } from '@testing-library/react';
-import App from '../components/ButtonPanel';
+import { render } from '@testing-library/react';
+import ButtonPanel from '../components/ButtonPanel';
 
-let getById;
-let getByClass;
-
-beforeAll(() => {
-  getById = queryByAttribute.bind(null, 'id');
-  getByClass = queryByAttribute.bind(null, 'class');
+test('renders AC button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/AC/i);
+  expect(textElement).toBeInTheDocument();
 });
 
-test('renders App component', () => {
-  const dom = render(<App />);
-  const calculator = getById(dom.container, 'calculator');
-  expect(calculator).toBeInTheDocument();
+test('renders +/- button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/\+\/-/i);
+  expect(textElement).toBeInTheDocument();
 });
 
-test('renders Display component', () => {
-  const dom = render(<App />);
-  const display = getByClass(dom.container, 'display');
-  expect(display).toBeInTheDocument();
+test('renders % button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/%/i);
+  expect(textElement).toBeInTheDocument();
 });
 
-test('renders ButtonPanel component', () => {
-  const dom = render(<App />);
-  const buttonPanel = getByClass(dom.container, 'button-panel');
-  expect(buttonPanel).toBeInTheDocument();
+test('renders ÷ button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/÷/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 7 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/7/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 8 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/8/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 9 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/9/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders × button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/×/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 4 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/4/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 5 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/5/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 6 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/6/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders - button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/^-$/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 1 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/1/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 2 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/2/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 3 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/3/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders + button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/^\+$/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders number 0 button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/0/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders . button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/\./i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders = button', () => {
+  const { getByText } = render(<ButtonPanel />);
+  const textElement = getByText(/=/i);
+  expect(textElement).toBeInTheDocument();
 });
